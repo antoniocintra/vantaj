6397feaf6d333453b7d1d788ddf56e86374a9f38
Antônio <aecf12@hotmail.com>
2026-04-05 20:47:23 -0300

    feat: add dependency injection to get user by id route

--- src/controllers/get-user-by-id.js
+++ src/controllers/get-user-by-id.js
@@ -1,4 +1,3 @@
-import { GetUserByIdUseCase } from '../use-cases/index.js'
 import {
     checkIfIdIsValid,
     invalidUserIdResponse,
@@ -8,6 +7,9 @@ import {
 } from './helpers/index.js'
 
 export class GetUserByIdController {
+    constructor(getUserByIdUseCase) {
+        this.getUserByIdUseCase = getUserByIdUseCase
+    }
     async execute(httpRequest) {
         try {
             const isIdValid = checkIfIdIsValid(httpRequest.params.userId)
@@ -15,9 +17,8 @@ export class GetUserByIdController {
             if (!isIdValid) {
                 return invalidUserIdResponse()
             }
-            const getUserByIdUseCase = new GetUserByIdUseCase()
 
-            const user = await getUserByIdUseCase.execute(
+            const user = await this.getUserByIdUseCase.execute(
                 httpRequest.params.userId,
             )
 
